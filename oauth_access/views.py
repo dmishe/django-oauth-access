4f381da2409b1524bd7ce4451b5b36c94fbcb541
Dmitry Shevchenko <dmishe@gmail.com>
2011-10-07 00:25:12 +0300

    fix session redirects

--- oauth_access/views.py
+++ oauth_access/views.py
@@ -19,7 +19,7 @@ def oauth_login(request, service,
             url_extra = "&display=touch"
         token = None
 
-    if hasattr(request, "session") and redirect_to_session_key in request.GET:
+    if hasattr(request, "session") and redirect_field_name in request.GET:
         request.session[redirect_to_session_key] = request.GET.get(redirect_field_name)
     return HttpResponseRedirect(access.authorization_url(token)+url_extra)
 
